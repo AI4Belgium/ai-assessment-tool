4dd2f131f63943a7d680a4f1805fd341742bf273
Robin Duqué <robin.duque@outlook.com>
2024-04-17 14:44:41 +0200

    fix linting

--- migrations/1713343362934_Migration.ts
+++ migrations/1713343362934_Migration.ts
@@ -1,11 +1,11 @@
 import { Db } from 'mongodb'
-import { MigrationInterface } from 'mongo-migrate-ts';
+import { MigrationInterface } from 'mongo-migrate-ts'
 
 export class Migration1713343362934 implements MigrationInterface {
-  public async up(db: Db): Promise<any> {
+  public async up (db: Db): Promise<any> {
     console.log('Migration1713343362934.up()')
   }
 
-  public async down(db: Db): Promise<any> {
+  public async down (db: Db): Promise<any> {
   }
 }
